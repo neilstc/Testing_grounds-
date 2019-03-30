1ca514f0f0647c6182e5a75ec164fa89152f116c
neil <neilmxstc@gmail.com>
2019-03-30 15:25:25 +0300

    nothing new in this commit

---
+++ Source/Grounds/ChooseNextWayPoint.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "ChooseNextWayPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GROUNDS_API UChooseNextWayPoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
